c8e135e77b05aca82b19f9fdd8bc6a36f33168cf
nohatte0403 <23020884@vnu.edu.vn>
2025-04-03 11:09:15 +0700

    Update
    Changes Animal, Wall, Grass, Bomber class

--- src/uet/oop/bomberman/entities/Entity.java
+++ src/uet/oop/bomberman/entities/Entity.java
@@ -13,18 +13,16 @@ public abstract class Entity {
     //Tọa độ Y tính từ góc trái trên trong Canvas
     protected int y;
 
+    protected Image img;
+
     public int getX() { return x; }
     public int getY() { return y; }
     public void setX(int x) { this.x = x; }
     public void setY(int y) { this.y = y; }
 
-    //Kiểm tra va trạm của entity với entity khác
-    public boolean isColliding(Entity other) {
-        return this.x == other.x && this.y == other.y;
-    }
+    public Entity(){
 
-
-    protected Image img;
+    };
 
     //Khởi tạo đối tượng, chuyển từ tọa độ đơn vị sang tọa độ trong canvas
     public Entity( int xUnit, int yUnit, Image img) {
@@ -33,19 +31,22 @@ public abstract class Entity {
         this.img = img;
     }
 
+    //Kiểm tra va trạm của entity với entity khác
+    public boolean isColliding(Entity other) {
+        return this.x == other.x && this.y == other.y;
+    }
+
+    public Image getImg() {
+        return img;
+    }
+
+    public void setImg(Image img) {
+        this.img = img;
+    }
+
     public void render(GraphicsContext gc) {
-        ImageView iv = new ImageView(img);
-        SnapshotParameters params = new SnapshotParameters();
-        params.setFill(Color.TRANSPARENT);
-        iv.setRotate(90); // Xoay 90 độ, có thể thay đổi theo hướng di chuyển
-        gc.drawImage(iv.snapshot(params, null), x, y);
+        gc.drawImage(img, x, y);
     }
-
-    @Override
-    public String toString() {
-        return "Entity at (" + x + ", " + y + ")";
-    }
-
 
     public abstract void update();
 }
